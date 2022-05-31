d6affa2cc32dd37fed9767c79c8f07532b06ca27
Wimal Pathmasiri <wimal@Wimals-MBP.lan>
2022-05-31 11:43:26 -0400

    Updated files

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -30,19 +30,44 @@ let map = L.map('mapid', {
 
 // Retrieve the earthquake GeoJSON data.
 d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+    function styleInfo(feature) {
+        return {
+            opacity: 1,
+            fillOpacity: 1,
+            fillColor: "#ffae42",
+            color: "#000000",
+            radius: getRadius(feature.properties.mag),
+            stroke: true,
+            weight: 0.5
+        };
+    }
 
-   // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data, {
-
-    // We turn each feature into a circleMarker on the map.
-    
-    pointToLayer: function(feature, latlng) {
-                console.log(feature, latlng);
-                return L.circleMarker(latlng);
-            },
-        }).addTo(map);
-    });
+    // This function determines the radius of the earthquake marker based on its magnitude.
+    // Earthquakes with a magnitude of 0 will be plotted with a radius of 1.
+    function getRadius(magnitude) {
+        if (magnitude === 0) {
+            return 1;
+        }
+        return magnitude * 4;
+    };
 
 
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data, {
+
+        // We turn each feature into a circleMarker on the map.
+
+        pointToLayer: function (feature, latlng) {
+            console.log(data);
+            return L.circleMarker(latlng)
+        },
+        // We set the style for each circleMarker using our styleInfo function.
+        style: styleInfo
+    }).addTo(map);
+});
+
+// This function returns the style data for each of the earthquakes we plot on
+// the map. We pass the magnitude of the earthquake into a function
+// to calculate the radius.
 
 L.control.layers(baseMaps).addTo(map);
