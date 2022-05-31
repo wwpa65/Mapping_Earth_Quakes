ee5be83e2bf7b57e21cd58a116989adfa813f216
Wimal Pathmasiri <wimal@Wimals-MBP.lan>
2022-05-31 10:00:55 -0400

    Updated files

--- Earthquakes_past7days/static/js/logicStep2.js
+++ Earthquakes_past7days/static/js/logicStep2.js
@@ -26,13 +26,22 @@ let map = L.map('mapid', {
     center: [39.5, -98.5],
     zoom: 3,
     layers: [streets]
-});
+})
 
 // Retrieve the earthquake GeoJSON data.
-d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
-  // Creating a GeoJSON layer with the retrieved data.
-  L.geoJSON(data).addTo(map);
-});
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+
+   // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data, {
+
+    // We turn each feature into a circleMarker on the map.
+    
+    pointToLayer: function(feature, latlng) {
+                console.log(feature, latlng);
+                return L.circleMarker(latlng);
+            },
+        }).addTo(map);
+    });
 
 
 
